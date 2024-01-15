be132370b0808e05344a357e522a18b1c12bf109
MichaelDesrosiers <michael.desrosiers@lunalabs.us>
2024-01-15 11:47:26 -0500

    Updates

--- MotionAccelerometer.ino
+++ MotionAccelerometer.ino
@@ -7,51 +7,99 @@
 
 #define PIN_TEMP    22
 #define PIN_MPU_INT 23
+#define PIN_SWITCH  24
 #define CLK_TMP     1
 #define CS_TMP      2
 #define DO_TMP      4
+
+#define REPORT_ON_SERIAL
+#define REPORT_ON_KEYBOARD
+#define TRIGGER_ON_SWITCH
+
+#define TRIGGER_COOLDOWN_MS 2000
 
 MicroDS18B20<PIN_TEMP> sensor;
 MAX6675 thermocouple(CLK_TMP, CS_TMP, DO_TMP);
 Adafruit_MPU6050 mpu;
 
-void Read_MPU_ISR() {
-  Serial.println("INTERRUPT");
+volatile bool triggered = false;
+volatile bool onCooldown = false;
+long lastTriggerTime;
+
+void ISR_MPU() {
+  #ifdef TRIGGER_ON_MOTION
+    Serial.println("INTERRUPT-MPU");
+    if (!onCooldown) {
+      triggered = true;
+    }
+  #endif
+}
+
+void ISR_SWITCH() {
+  #ifdef TRIGGER_ON_SWITCH
+    Serial.println("INTERRUPT-SWITCH");
+    if (!onCooldown) {
+      triggered = true;
+    }
+  #endif 
 }
 
 void reportData(float t1, float t2) {
-  Keyboard.print(t1);
-  Keyboard.press(KEY_TAB);
-  Keyboard.releaseAll();
-  Keyboard.print(t2);
-  Keyboard.press(KEY_KP_ENTER);
-  Keyboard.releaseAll();
+  #ifdef REPORT_ON_SERIAL
+    Serial.print(t1); Serial.print("\t"); Serial.println(t2);
+  #endif
+
+  #ifdef REPORT_ON_KEYBOARD
+    Keyboard.print(t1);
+    Keyboard.press(KEY_TAB);
+    Keyboard.releaseAll();
+    Keyboard.print(t2);
+    Keyboard.press(KEY_KP_ENTER);
+    Keyboard.releaseAll();  
+  #endif
 }
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  while (!Serial) {
+    delay(10);
+  }
   
-  mpu.begin();
-  // Initialize MPU6050 and configure it for motion detection interrupt
-  mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
-  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
-  mpu.setMotionDetectionThreshold(10); // Set your desired motion detection threshold
-  mpu.setMotionDetectionDuration(5); // Set the duration for motion detection
-  mpu.setInterruptPinLatch(true);
-  mpu.setInterruptPinPolarity(true);
-  mpu.setMotionInterrupt(true);
+  
+  #ifdef TRIGGER_ON_MOTION
+    // Configure accelerometer for motion interrupt
+    mpu.begin();  
+    mpu.setHighPassFilter(MPU6050_HIGHPASS_0_63_HZ);
+    mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+    mpu.setMotionDetectionThreshold(10);
+    mpu.setMotionDetectionDuration(5);
+    mpu.setInterruptPinLatch(true);
+    mpu.setInterruptPinPolarity(true);
+    mpu.setMotionInterrupt(true);
+    attachInterrupt(digitalPinToInterrupt(PIN_MPU_INT), ISR_MPU, RISING); 
+  #endif
 
-  attachInterrupt(digitalPinToInterrupt(PIN_MPU_INT), Read_MPU_ISR, RISING); // Trigger interrupt on rising edge
+  #ifdef TRIGGER_ON_SWITCH
+    // Configure switch for interrupt
+    pinMode(PIN_SWITCH, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_SWITCH), ISR_SWITCH, FALLING);
+  #endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   sensor.requestTemp();
-  float temperatureLocal = sensor.getTemp();
-  float temperatureRemote = thermocouple.readCelsius();
-  reportData(temperatureLocal, temperatureRemote);
+  if (triggered) {
+    lastTriggerTime = millis();
+    onCooldown = true;
+    float temperatureLocal = sensor.getTemp();
+    float temperatureRemote = thermocouple.readCelsius();
+    reportData(temperatureLocal, temperatureRemote);
+  }
 
-  delay(1000);
+  if (onCooldown) {
+    if ((millis() - lastTriggerTime) > TRIGGER_COOLDOWN_MS) {
+      onCooldown = false;
+    }
+  }
 }
